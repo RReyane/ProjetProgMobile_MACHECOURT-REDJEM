095262f71f9bcf04e10f055299f8d37f0a0dfb42
Redjem Reyane <reyane.redjem@univ-lyon2.fr>
2024-04-19 19:50:01 +0200

    ajout d'asset pour la barre de vie, ajout d'un menu de fin de partie, d'une barre de vie, d'un compteur de balle et de l'ajout d'une balle tout les 5 tours, de l'apparition de monstre ainsi que leur collisions avec la balle, la disparition de monstre en effectuant des dégâts au joueur

--- ProjetProgMobile_MACHECOURT-REDJEM/ViewController.swift
+++ ProjetProgMobile_MACHECOURT-REDJEM/ViewController.swift
@@ -8,26 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var FinPartie = false
+    var canonOriginPos: CGPoint!
+    var kartOriginPos: CGPoint!
+    var balleOriginPos: CGPoint!
+    
+    @IBOutlet var MenuFinDePartie: [UILabel]!
+    @IBOutlet weak var BoutonFinDePartie: UIButton!
     
     var cible:CGPoint! // endroit visé par le doigt
     var radians:CGFloat! // angle entre le canon et la cible
+    
     @IBOutlet var Joueur: [UIImageView]! //liste Joueur 0:canon,1:kart
+    var pvJoueur = 0
+    var maxPV = 10
+    @IBOutlet weak var pvBarreInterieur: UIImageView!
     
     var t: Timer!
-    var tempsEcoule = 0.0 // temps ecoulé durant chaque tour
+//    var tempsEcoule = 0.0 // temps ecoulé durant chaque tour
     var vitesse = 15.0
     var velocity: CGPoint! // velocité de la balle
     var isBallmoving = false // drapeau mouvement balle
     var balleMax = 1 // nombre de balle max a lancer
     var balleRestante = 1 //nombre de balle restante a lancé
+    @IBOutlet weak var cptBalle: UILabel!
+    
+    var didMonsterMoved = true
     
     @IBOutlet weak var ballImage: UIImageView!
     struct monstre { //structure de monstre, une image et des PV
         var image: UIImageView
         var pv = 1
-        var attackType = 1 //0= corpsAcorps, 1 = distance
+        var attackType = 0 //0= corpsAcorps, 1 = distance
     }
-    var listeMonstres: [monstre]! //liste de monstre
+    var listeMonstres: [monstre]! = [] //liste de monstre
     
     @IBOutlet weak var numVague: UILabel!
     var cptVague = 0
@@ -41,6 +55,37 @@ class ViewController: UIViewController {
         p.x += velocity.x
         p.y += velocity.y
         ballImage.frame.origin = p
+        
+        var tempIndex = 0
+        for m in listeMonstres {
+            if p.x + s.width > m.image.frame.origin.x && p.x + s.width < m.image.frame.origin.x + m.image.frame.width && p.y + s.height > m.image.frame.origin.y && p.y + s.height < m.image.frame.origin.y + m.image.frame.height {
+                m.image.isHidden = true
+                listeMonstres.remove(at: tempIndex)
+                tempIndex = tempIndex - 1
+                // Les rebonds ne fonctionnent pas sur les monstres car on prend toujours le premier test...
+                //if p.x + s.width > m.image.frame.origin.x || p.x < m.image.frame.origin.x + m.image.frame.width {
+                //    velocity.x = -velocity.x
+                //    print("X")
+                //}
+                //if p.y + s.height > m.image.frame.origin.y || p.y < m.image.frame.origin.y + m.image.frame.height {
+                //    velocity.y = -velocity.y
+                //    print("Y")
+                //}
+                let monsterCenterX = m.image.frame.origin.x + m.image.frame.size.width / 2
+                let monsterCenterY = m.image.frame.origin.y + m.image.frame.size.height / 2
+                
+                let collisionDirectionX = ballImage.center.x - monsterCenterX
+                let collisionDirectionY = ballImage.center.y - monsterCenterY
+                
+                if abs(collisionDirectionX) > abs(collisionDirectionY){
+                    velocity.x = -velocity.x
+                } else {
+                    velocity.y = -velocity.y
+                }
+                                
+            }
+            tempIndex = tempIndex + 1
+        }
         
         if p.x + s.width > e.width || p.x < e.minX { //rebond sur un mur vertical
             velocity.x = -velocity.x
@@ -64,6 +109,8 @@ class ViewController: UIViewController {
                 ballImage.isHidden = true
                 isBallmoving = false
                 
+                didMonsterMoved = false // Setting it back to false so that they move again on the next round. It needs to be synchronised with the ball otherwise monsters will move much too fast.
+                
                 cptVague += 1
                 numVague.text = String(cptVague)
                 
@@ -85,37 +132,75 @@ class ViewController: UIViewController {
     }
     
     @objc func loop(t:Timer){
-        if isBallmoving {
-            move()
-
-//            vitesse += 0.05 * Double(tempsEcoule)
-//            tempsEcoule += 0.05
+        if !FinPartie{
+            if isBallmoving {
+                move()
+                
+                //            vitesse += 0.05 * Double(tempsEcoule)
+                //            tempsEcoule += 0.05
+            }
+            if !didMonsterMoved {
+                if cptVague % 5 == 0 {
+                    balleMax += 1
+                    cptBalle.text = "x"+String(balleMax)
+                }
+                if !listeMonstres!.isEmpty {
+                    didMonsterMoved = true
+                    var tempIndex = 0
+                    for monster in listeMonstres {
+                        monster.image.frame.origin.y = monster.image.frame.origin.y + 47 // -20 should be adjust to the size of a tile.
+                        if monster.image.frame.origin.y >= Joueur[1].frame.origin.y-5 {
+                            pvJoueur -= 1
+                            print("pv du joueur \(pvJoueur)")
+                            monster.image.isHidden = true
+                            listeMonstres.remove(at: tempIndex)
+                            tempIndex-=1
+                        }
+                        tempIndex += 1
+                    }
+                }
+                affichePV()
+                if pvJoueur <= 0 {
+                    FinPartie = true
+                }
+                spawnRandomEnnemies() // Spawning new monster(s)
+            }
+            
+            //        else{
+            //            tempsEcoule = 0 // remise du timer a 0
+            //            vitesse = 15 // remise de la vitesse a sa valeur originel
+            //        }
         }
-//        else{
-//            tempsEcoule = 0 // remise du timer a 0
-//            vitesse = 15 // remise de la vitesse a sa valeur originel
-//        }
+        else {
+            MenuFinDePartie[2].text = numVague.text
+            for element in MenuFinDePartie{
+                element.isHidden = false
+            }
+            BoutonFinDePartie.isHidden = false
+            BoutonFinDePartie.isEnabled = true
+        }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        canonOriginPos = Joueur[0].frame.origin
+        kartOriginPos = Joueur[1].frame.origin
+        balleOriginPos = ballImage.frame.origin
         // Do any additional setup after loading the view.
-
+        spawnRandomEnnemies()
         t = Timer.scheduledTimer(timeInterval: 0.05, target: self, selector: #selector(loop), userInfo: nil, repeats: true) // initialisation de la boucle de jeu
     }
-
-//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        let doigt = touches.randomElement()!
-//        let point = doigt.location(in: view)
-//        print("vous avez touché a la position x:\(point.x) y:\(point.y)")
-//    }
-
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        Joueur[0].center.x = ballImage.center.x //Fix d'un bug lors de la translation du joueur, ça nouvelle position n'est pas prise en compte (a cause de l'utilisation de self peut etre ?) NE PAS TOUCHER
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
         
         let doigt = touches.randomElement()!
         cible = doigt.location(in: view)
-        if !isBallmoving {
+        if !isBallmoving && !FinPartie {
+            
+            Joueur[0].center.x = ballImage.center.x //Fix d'un bug lors de la translation du joueur, ça nouvelle position n'est pas prise en compte (a cause de l'utilisation de self peut etre ?) NE PAS TOUCHER
+            
+            
             let dX = cible!.x-Joueur[0].center.x // distance X entre la cible et le canon
             let dY = cible!.y-Joueur[0].center.y // distance Y entre la cible et le canon
             radians = atan2(dY, dX) + .pi/2 // calcul de l'angle entre la cible et le canon
@@ -126,14 +211,34 @@ class ViewController: UIViewController {
             if radians < -1 || radians > 3 {
                 radians = -1
             }
-
+            
+            Joueur[0].transform = CGAffineTransformMakeRotation(radians) // rotation du canon vers la cible
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        let doigt = touches.randomElement()!
+        cible = doigt.location(in: view)
+        if !isBallmoving && !FinPartie{
+            let dX = cible!.x-Joueur[0].center.x // distance X entre la cible et le canon
+            let dY = cible!.y-Joueur[0].center.y // distance Y entre la cible et le canon
+            radians = atan2(dY, dX) + .pi/2 // calcul de l'angle entre la cible et le canon
+            // limitation de l'angle max et min
+            if radians > 1 && radians < 3 {
+                radians = 1
+            }
+            if radians < -1 || radians > 3 {
+                radians = -1
+            }
+            
             Joueur[0].transform = CGAffineTransformMakeRotation(radians) // rotation du canon vers la cible
         }
         
     }
     
     override func touchesEnded(_ touches: Set<UITouch>,     with event: UIEvent?) {
-        if !isBallmoving {
+        if !isBallmoving && !FinPartie{
             if radians != nil {
                 let distance  = getDistanceBalleCanon(Joueur[0].bounds.size)
                 let ballX = Joueur[0].center.x + CGFloat(cos(radians - .pi/2)) * distance
@@ -153,6 +258,7 @@ class ViewController: UIViewController {
         let nomsMonstre = ["Punball_Ennemi_PeaLauncher","PunBall_Ennemi_Skeleton"]
         
         var nbMonstre: Int!
+        var available_position = [0, 0, 0, 0, 0, 0]
         
         switch Int.random(in: 0...10) {
         case 5...8:
@@ -163,14 +269,74 @@ class ViewController: UIViewController {
             nbMonstre = 1
         }
         
-//        for _ in 1...nbMonstre!{
-//            listeMonstres.append(monstre(image: UIImageView(image: UIImage(named: nomsMonstre[0]))))
-//        }
+        for _ in 1...nbMonstre!{
+            let m = monstre(image: UIImageView(image: UIImage(named: nomsMonstre[0])))
+            var random = Int.random(in: 1...6)
+            while available_position[random-1] == 1 { // Assuring that multiple monsters don't have the same position when spawning
+                random = Int.random(in: 1...6)
+            }
+            available_position[random-1] = 1
+            
+            m.image.frame.origin = CGPoint(x: 9+47*random, y: 238) // Defining the position of new monsters
+            m.image.frame.size = CGSize(width:47, height: 47)
+            listeMonstres.append(m)
+            displayMonsters()
+            print("New Monster(s) spawned \(m.image.frame.origin)") // Only here to debug
+        }
     }
-}
-
-func getDistanceBalleCanon(_ J:CGSize) -> Double{
-    let joueurHeight = J.height / 2.0
-    let joueurWidth = J.width / 2.0
-    return sqrt(pow(joueurHeight,2.0)+pow(joueurWidth,2.0)) - 25.0// distance entre la balle et le centre du cannon
+    
+    func displayMonsters(){
+        for monster in listeMonstres {
+//            self.view.addSubview(monster.image)
+            self.view.insertSubview(monster.image,belowSubview: MenuFinDePartie[0])
+        }
+    }
+    
+    func getDistanceBalleCanon(_ J:CGSize) -> Double{
+        let joueurHeight = J.height / 2.0
+        let joueurWidth = J.width / 2.0
+        return sqrt(pow(joueurHeight,2.0)+pow(joueurWidth,2.0)) - 25.0// distance entre la balle et le centre du cannon
+    }
+    
+    func affichePV(){
+        let PourcentageDeVie = CGFloat(pvJoueur) / CGFloat(maxPV)
+        let newWidth = pvBarreInterieur.frame.width * PourcentageDeVie
+        let scaleFactor = newWidth / pvBarreInterieur.frame.width
+        pvBarreInterieur.transform = CGAffineTransform(scaleX: CGFloat(scaleFactor), y:1.0)
+//        pvBarreInterieur.frame = CGRect(x: pvBarreInterieur.frame.origin.x, y: pvBarreInterieur.frame.origin.y,width: newWidth, height: pvBarreInterieur.frame.height)
+        
+    }
+    
+    @IBAction func BoutonFinDePartie(_ sender: Any) {
+        Joueur[0].frame.origin = canonOriginPos
+        Joueur[1].frame.origin = kartOriginPos
+        ballImage.frame.origin = balleOriginPos
+        pvJoueur = 10
+        
+        numVague.text = "0"
+        cptVague = 0
+        
+        balleMax = 1
+        balleRestante = 1
+        cptBalle.text = "x"+String(balleMax)
+        
+        pvJoueur = 10
+        
+        for m in listeMonstres{
+            m.image.isHidden = true
+        }
+        listeMonstres = []
+        displayMonsters()
+        
+        for element in MenuFinDePartie{
+            element.isHidden = true
+        }
+        BoutonFinDePartie.isHidden = true
+        BoutonFinDePartie.isEnabled = false
+        
+        affichePV()
+        spawnRandomEnnemies()
+        
+        FinPartie = false
+    }
 }
